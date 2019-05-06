4031036abec15c67cea020aa6c330f7c0a6ff01f
David Gil Baeza <davidgil.univ@gmail.com>
2019-05-06 12:30:35 +0200

    Arreglos

--- src/main/java/services/MiscellaneousDataService.java
+++ src/main/java/services/MiscellaneousDataService.java
@@ -1,5 +1,6 @@
 package services;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,17 +13,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.Validator;
 
 import domain.Curriculum;
-import domain.Rookie;
 import domain.MiscellaneousData;
-import domain.PersonalData;
-import domain.PositionData;
-import repositories.CurriculumRepository;
+import domain.Rookie;
 import repositories.MiscellaneousDataRepository;
 
 @Service
 @Transactional
 public class MiscellaneousDataService {
-	
+
 	@Autowired
 	private MiscellaneousDataRepository miscellaneousDataRepository;
 	@Autowired
@@ -31,30 +29,31 @@ public class MiscellaneousDataService {
 	private CurriculumService curriculumService;
 	@Autowired
 	private Validator validator;
-	
+
 	public void save(MiscellaneousData m) {
 		this.miscellaneousDataRepository.save(m);
 	}
-	
+
 	public MiscellaneousData findOne(int miscellaneousDataId) {
 		return this.miscellaneousDataRepository.findOne(miscellaneousDataId);
 	}
 
 	public MiscellaneousData getMiscellaneousDataOfLoggedRookie(int miscellaneousDataId) {
 		Rookie rookie = this.rookieService.securityAndRookie();
-		MiscellaneousData miscellaneousData = this.miscellaneousDataRepository.getMiscellaneousDataOfRookie(rookie.getId(), miscellaneousDataId);
+		MiscellaneousData miscellaneousData = this.miscellaneousDataRepository
+				.getMiscellaneousDataOfRookie(rookie.getId(), miscellaneousDataId);
 		Assert.notNull(miscellaneousData);
 		return miscellaneousData;
 	}
-	
+
 	public void deleteInBatch(Iterable<MiscellaneousData> entities) {
 		this.miscellaneousDataRepository.deleteInBatch(entities);
 	}
 
 	public void addOrUpdateMiscellaneousDataAsRookie(MiscellaneousData miscellaneousData, int curriculumId) {
 		Rookie rookie = this.rookieService.securityAndRookie();
-		
-		if(miscellaneousData.getId()==0) {
+
+		if (miscellaneousData.getId() == 0) {
 			Curriculum curriculum = this.curriculumService.getCurriculumOfRookie(rookie.getId(), curriculumId);
 			Assert.notNull(curriculum);
 			List<MiscellaneousData> miscellaneoussData = curriculum.getMiscellaneousData();
@@ -62,7 +61,8 @@ public class MiscellaneousDataService {
 			this.curriculumService.save(curriculum);
 			this.curriculumService.flush();
 		} else {
-			Assert.notNull(this.miscellaneousDataRepository.getMiscellaneousDataOfRookie(rookie.getId(), miscellaneousData.getId()));
+			Assert.notNull(this.miscellaneousDataRepository.getMiscellaneousDataOfRookie(rookie.getId(),
+					miscellaneousData.getId()));
 			this.save(miscellaneousData);
 			this.flush();
 		}
@@ -74,8 +74,8 @@ public class MiscellaneousDataService {
 
 	public MiscellaneousData reconstruct(MiscellaneousData miscellaneousData, BindingResult binding) {
 		MiscellaneousData miscellaneousDataReconstruct = new MiscellaneousData();
-		
-		if(miscellaneousData.getId()==0) {
+
+		if (miscellaneousData.getId() == 0) {
 			miscellaneousDataReconstruct.setFreeText(miscellaneousData.getFreeText());
 			miscellaneousDataReconstruct.setAttachments(new ArrayList<String>());
 		} else {
@@ -85,15 +85,16 @@ public class MiscellaneousDataService {
 			miscellaneousDataReconstruct.setFreeText(miscellaneousData.getFreeText());
 			miscellaneousDataReconstruct.setAttachments(miscellaneousDataFounded.getAttachments());
 		}
-		
+
 		this.validator.validate(miscellaneousDataReconstruct, binding);
-		
+
 		return miscellaneousDataReconstruct;
 	}
 
 	public void deleteMiscellaneousDataAsRookie(int miscellaneousDataId) {
 		Rookie rookie = this.rookieService.securityAndRookie();
-		Assert.notNull(this.miscellaneousDataRepository.getMiscellaneousDataOfRookie(rookie.getId(), miscellaneousDataId));
+		Assert.notNull(
+				this.miscellaneousDataRepository.getMiscellaneousDataOfRookie(rookie.getId(), miscellaneousDataId));
 		MiscellaneousData miscellaneousData = this.findOne(miscellaneousDataId);
 		Curriculum curriculum = this.curriculumService.getCurriculumOfMiscellaneousData(miscellaneousDataId);
 		List<MiscellaneousData> miscellaneoussData = curriculum.getMiscellaneousData();
@@ -104,8 +105,11 @@ public class MiscellaneousDataService {
 	}
 
 	public void addAttachmentAsRookie(int miscellaneousDataId, String attachment) {
+		Assert.isTrue(this.isUrl(attachment));
+
 		Rookie rookie = this.rookieService.securityAndRookie();
-		MiscellaneousData miscellaneousData = this.miscellaneousDataRepository.getMiscellaneousDataOfRookie(rookie.getId(), miscellaneousDataId);
+		MiscellaneousData miscellaneousData = this.miscellaneousDataRepository
+				.getMiscellaneousDataOfRookie(rookie.getId(), miscellaneousDataId);
 		Assert.notNull(miscellaneousData);
 		Assert.isTrue(!attachment.contentEquals(""));
 		Assert.notNull(attachment);
@@ -116,7 +120,8 @@ public class MiscellaneousDataService {
 
 	public void deleteAttachmentAsRookie(int miscellaneousDataId, int attachmentIndex) {
 		Rookie rookie = this.rookieService.securityAndRookie();
-		MiscellaneousData miscellaneousData = this.miscellaneousDataRepository.getMiscellaneousDataOfRookie(rookie.getId(), miscellaneousDataId);
+		MiscellaneousData miscellaneousData = this.miscellaneousDataRepository
+				.getMiscellaneousDataOfRookie(rookie.getId(), miscellaneousDataId);
 		Assert.notNull(miscellaneousData);
 		List<String> attachments = miscellaneousData.getAttachments();
 		Assert.isTrue(attachmentIndex < attachments.size());
@@ -128,4 +133,14 @@ public class MiscellaneousDataService {
 	public List<String> getAttachmentsOfMiscellaneousDataOfLoggedRookie(int miscellaneousDataId) {
 		return this.getMiscellaneousDataOfLoggedRookie(miscellaneousDataId).getAttachments();
 	}
+
+	public Boolean isUrl(String url) {
+		try {
+			new URL(url).toURI();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
 }
