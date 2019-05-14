fb7cde8cfdd865492896ddaa47ed0162a01acb1f
David Gil Baeza <davidgil.univ@gmail.com>
2019-05-14 14:14:36 +0200

    Arreglo wrong urls

--- src/main/java/controllers/ItemProviderController.java
+++ src/main/java/controllers/ItemProviderController.java
@@ -16,20 +16,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import services.ItemService;
-import services.ProviderService;
 import domain.Item;
 import forms.FormObjectItem;
+import services.ItemService;
+import services.ProviderService;
 
 @Controller
 @RequestMapping("/item/provider")
 public class ItemProviderController extends AbstractController {
 
 	@Autowired
-	private ItemService		itemService;
+	private ItemService itemService;
 	@Autowired
-	private ProviderService	providerService;
-
+	private ProviderService providerService;
 
 	public ItemProviderController() {
 		super();
@@ -132,7 +131,8 @@ public class ItemProviderController extends AbstractController {
 	}
 
 	@RequestMapping(value = "/save", method = RequestMethod.POST, params = "save")
-	public ModelAndView saveItem(@ModelAttribute("formObjectItem") @Valid FormObjectItem formObjectItem, BindingResult binding) {
+	public ModelAndView saveItem(@ModelAttribute("formObjectItem") @Valid FormObjectItem formObjectItem,
+			BindingResult binding) {
 		ModelAndView result;
 		try {
 			String tiles;
@@ -150,20 +150,28 @@ public class ItemProviderController extends AbstractController {
 			// Links URL
 			if (!formObjectItem.getLinks().isEmpty())
 				for (String s : item.getLinks())
-					if (!this.itemService.isUrl(s))
+					if (!this.itemService.isUrl(s)) {
 						if (locale.contains("ES"))
-							binding.addError(new FieldError("formObjectItem", "links", formObjectItem.getLinks(), false, null, null, "URLs incorrecta"));
+							binding.addError(new FieldError("formObjectItem", "links", formObjectItem.getLinks(), false,
+									null, null, "URLs incorrecta"));
 						else
-							binding.addError(new FieldError("formObjectItem", "links", formObjectItem.getLinks(), false, null, null, "Wrong URLs"));
+							binding.addError(new FieldError("formObjectItem", "links", formObjectItem.getLinks(), false,
+									null, null, "Wrong URLs"));
+						break;
+					}
 
 			// Links URL
 			if (!formObjectItem.getPictures().isEmpty())
 				for (String s : item.getPictures())
-					if (!this.itemService.isUrl(s))
+					if (!this.itemService.isUrl(s)) {
 						if (locale.contains("ES"))
-							binding.addError(new FieldError("formObjectItem", "pictures", formObjectItem.getPictures(), false, null, null, "URLs incorrecta"));
+							binding.addError(new FieldError("formObjectItem", "pictures", formObjectItem.getPictures(),
+									false, null, null, "URLs incorrecta"));
 						else
-							binding.addError(new FieldError("formObjectItem", "pictures", formObjectItem.getPictures(), false, null, null, "Wrong URLs"));
+							binding.addError(new FieldError("formObjectItem", "pictures", formObjectItem.getPictures(),
+									false, null, null, "Wrong URLs"));
+						break;
+					}
 
 			if (binding.hasErrors())
 				result = this.createEditModelAndView(tiles, formObjectItem);
